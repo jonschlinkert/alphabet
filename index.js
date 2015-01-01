90d96c749e83dac566fabee800574a45cb4e362e
jonschlinkert <jon.schlinkert@sellside.com>
2014-12-31 19:57:00 -0500

    update copyright year

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * alphabet <https://github.com/jonschlinkert/alphabet>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT license.
  */
 
