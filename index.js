30c91a1b22e0f3f069d00277a2c9f77a6a8a673f
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-09 04:51:07 -0400

    remove usseless comment, minify var names

--- index.js
+++ index.js
@@ -7,21 +7,9 @@
 
 'use strict';
 
-var lower = 'abcdefghijklmnopqrstuvwxyz';
-var upper = lower.toUpperCase();
+var l = 'abcdefghijklmnopqrstuvwxyz';
+var u = l.toUpperCase();
 
-/*
-Why `+` instead of template literal?
-Because we're concatenating vars, so there's no benefit.
-Also because of retro-compatibility.
-
-Why `split` instead of `[...]`?
-Because `split` is "more readable", and is retro-compatible.
-It also has better performance:
-It iterates over fixed-size code-units, instead of var-len code-points.
-This has the advantage of explicitly telling the engine "this is an array of bytes, not pointers to arbitrary Unicode strs",
-which in turn, reduces memory use, because of contiguous ASCII chars, and makes indexing faster.
-*/
-module.exports = (upper + lower).split('');
-module.exports.lower = lower.split('');
-module.exports.upper = upper.split('');
+module.exports = (u + l).split('');
+module.exports.lower = l.split('');
+module.exports.upper = u.split('');
